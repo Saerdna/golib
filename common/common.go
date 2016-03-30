a183535ad3149a0d0c4ef7753d1b2772cb90fc49
Saerdna <zhaodahao@gmail.com>
2016-03-30 17:17:08 +0800

    add common

---
+++ common/common.go
@@ -0,0 +1,20 @@
+package common
+
+import (
+	"net"
+)
+
+func GetLocalIp() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "127.0.0.1"
+	}
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return "127.0.0.1"
+}
